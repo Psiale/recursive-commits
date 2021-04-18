8aa0a96e83caab72d2eb4af2c25a3a3c8b6ed8d4
Alexis Sanchez <psialedev@gmail.com>
2021-04-17 20:34:34 -0500

    Working on the api call

--- src/redux/actions/commits.js
+++ src/redux/actions/commits.js
@@ -1,8 +1,20 @@
+import { getRequest } from '../../api/helpers';
 import SHOW_COMMITS from '../constants/commits'
+import setError from './error';
+import setFetching from './fetching';
 
 const showCommits = commits => ({
     type: SHOW_COMMITS,
     commits,
 });
+
+const getCommits = endpoint => async dispatch => {
+    setFetching(true)
+    try {
+        const res = await getRequest()
+    } catch (error) {
+        
+    }
+} 
 
 export default showCommits
