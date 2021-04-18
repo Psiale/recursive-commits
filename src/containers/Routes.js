bf71f6a9fa93d0c3b81cf473b3dcde526eb052f1
Alexis Sanchez <psialedev@gmail.com>
2021-04-17 21:38:17 -0500

    Installed react-dotenv to handle github password

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { HashRouter, Switch, Route } from 'react-router-dom';
+import env from 'react-dotenv'
 import PropTypes from 'prop-types'
 import Fun from './Fun'
 import Home from './Home'
 import History from './History'
 import { setHeader } from '../api/helpers';
-
+import axios from 'axios';
 const Routes = () => {
-    setHeader()
+    setHeader(env.API_KEY)
+    //Testing correct password assignation, this and the axios library
+    // will be deleted
+    console.log(axios.defaults)
 }
