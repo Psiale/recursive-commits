100fbb81c94dac9b53eff251770ad942d3271204
Alexis Sanchez <psialedev@gmail.com>
2021-04-18 16:53:28 -0500

    Still having issues with dotenv and the apikey

--- public/env.js
+++
@@ -1,3 +0,0 @@
-window.env = {
-  "API_KEY": "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"
-};
--- src/containers/History.js
+++ src/containers/History.js
@@ -1,15 +1,45 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { commitsEndPoint } from '../redux/constants/index'
+import PropTypes from "prop-types";
+import getCommits from "../redux/actions/commits"
 
-const History = () => {
-const history = useHistory();
-return (
+const History = ({ commits, getCommits }) => {
+	useEffect(() => {
+		getCommits(commitsEndPoint);
+	}, [commits]);
+  const history = useHistory();
+  return (
     <>
-    <p>Hello from History</p>
-    <button onClick={() => history.push('/')}> home </button>
-    <button onClick={() => history.push('/fun')}> fun </button>
+      {(commits && commits.length > 0)
+			? (commits.map((commit) => {
+				return (
+					<>
+					<div className="">
+						<p>{commit.message}</p>
+					</div>
+					</>
+				)
+			}))
+			: (<p>no commits yet</p>)}
+      <button onClick={() => history.push("/")}> home </button>
+      <button onClick={() => history.push("/fun")}> fun </button>
     </>
-    )
-}
+  );
+};
 
-export default History;
+History.propTypes = {
+  commits: PropTypes.array.isRequired,
+	getCommits: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+	commits: state.commitStore,
+});
+
+const mapDispatchToProps = dispatch => ({
+	getCommits: () => dispatch(getCommits()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(History);
