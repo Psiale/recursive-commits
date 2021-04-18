2433d1239061c35c33fa2416e7bd4e0f93ac1bdf
Alexis Sanchez <psialedev@gmail.com>
2021-04-17 21:52:12 -0500

    Finishing DAY 1: Debug error with dotenv library

---
+++ src/containers/Fun.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Fun = () => (
+    <>
+    <p>Hello from Fun</p>
+    </>
+)
+
+export default Fun;
---
+++ src/containers/History.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const History = () => (
+    <>
+    <p>Hello from History</p>
+    </>
+)
+
+export default History;
