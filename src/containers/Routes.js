3160f9bf3cf6c45559656aa7b4e6dbf4eec75075
Alexis Sanchez <psialedev@gmail.com>
2021-04-17 21:44:51 -0500

    Finished Routes container

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import env from 'react-dotenv'
-import PropTypes from 'prop-types'
 import Fun from './Fun'
 import Home from './Home'
 import History from './History'
@@ -12,4 +11,20 @@ const Routes = () => {
     //Testing correct password assignation, this and the axios library
     // will be deleted
     console.log(axios.defaults)
+    return (
+        <>
+        <HashRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/commit-history" component={History} />
+            <Route path="/fun" component={Fun} />
+          </Switch>
+        </HashRouter>
+        </>
+    )
 }
+
+export default Routes;
+
+
+
