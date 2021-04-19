862b2e0af52f4504317b0128745df3ebdafebea0
Alexis Sanchez <psialedev@gmail.com>
2021-04-18 23:14:48 -0500

    Finishing DAY 2: Do user api call and user component

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import { BASE_URL } from '../redux/constants/index';
+import { COMMIT_BASE_URL } from '../redux/constants/index';
 
-export const getRequest = async endpoint => axios.get(`${BASE_URL}${endpoint}`);
+export const getRequest = async endpoint => axios.get(`${COMMIT_BASE_URL}${endpoint}`);
 
 export const setHeader = password => {
     axios.defaults.headers.common = {
--- src/redux/constants/index.js
+++ src/redux/constants/index.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'https://api.github.com/repos/Psiale/';
+export const COMMIT_BASE_URL = 'https://api.github.com/repos/Psiale/';
+export const USER_BASE_URL = 'https://api.github.com/users/Psiale'
 export const commitsEndPoint = 'recursive-commits/commits?per_page=100';
